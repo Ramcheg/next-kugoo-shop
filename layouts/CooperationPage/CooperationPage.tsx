c2858c6391cad41bc3692f97e0437845ee6f777a
Vlad <qwerasvlad@gmail.com>
2023-09-07 16:32:37 +0300

    Service page finished

---
+++ layouts/CooperationPage/CooperationPage.tsx
@@ -0,0 +1,12 @@
+import { CooperationBanner } from "@/components/CooperationPage";
+import { FC } from "react";
+
+export const CooperationPage: FC = () => {
+    return (
+        <>
+            <section className="mt-4 md:mt-8">
+                <CooperationBanner />
+            </section>
+        </>
+    );
+};
